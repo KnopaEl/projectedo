f2936a66423a82e65389e7057864fd1f71ec6554
Elizabeth <43093955+KnopaEl@users.noreply.github.com>
2019-11-28 23:01:59 +0300

    Создание сайта

---
+++ blocks/footer.php
@@ -0,0 +1,22 @@
+<footer class="page-footer font-small blue pt-4 footer-color">
+  <div class="container-fluid text-center text-md-left">
+    <div class="row">
+      <div class="col-md-6 mt-md-0 mt-3">
+        <h5 class="text-uppercase">Дополнительная информация</h5>
+        <p><strong>Работу выполнил студент:</strong>	Мельник Елизавета Васильевна</p>
+        <p><strong>№ группы:</strong>	171-341</p>
+        <p><strong>Руководитель:</strong>	Кесель Сергей Александрович</p>
+        <p><strong>E-mail:</strong>	<a href="#!">lizabetmelnik@gmail.com</a></p>
+      </div>
+      <hr class="clearfix w-100 d-md-none pb-3">
+      <div class="col-md-6 mb-md-0 mb-3">
+        <h5 class="text-uppercase"><a href="https://github.com/KnopaEl/projectedo">Ссылка на репозиторий </a></h5>
+        <ul class="list-unstyled">
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright text-center py-3">© 2019-2020:
+    <a href="https://mospolytech.ru/index.php">Московский Политехнический Университет.</a>
+  </div>
+</footer>
